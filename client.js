093ef35e3bf030d975c91cdcc1372c9dc6a0b0e5
gane5hvarma <raghavag772@gmail.com>
2024-08-21 10:01:06 +0530

    autoscale sockets based on cpu

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const { io }  =  require("socket.io-client");
 const server_url = process.env.SERVER_URL || 'ws://localhost:3000'
 const socket = io(server_url, {transports: ['websocket']})
 
 socket.on("connect", () => {
     console.log("Connected to server");
-    for(i=0;i<1000;i++){ // emit 1000 events
+    for(let i=0;i<1000;i++){ // emit 1000 events
         socket.emit('sampleEndpoint', { someData: 'example' }, (response) => {
-            console.log('Server response:',  response);
+            console.log('Server response:', i,   response);
         });
     }
 });
